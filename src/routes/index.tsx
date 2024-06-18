f26ff0dd1bb15dbc9450b30f73b2db285d4b9eb9
pspencer-arculus <pspencer@arculus.co>
2024-06-18 14:33:41 -0600

    compute units staging ground

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,41 @@
+import { createTransferCheckedInstruction } from "@solana/spl-token";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 import { createAsync } from "@solidjs/router";
 import { For, Suspense } from "solid-js";
 import Counter from "~/components/Counter";
-import { getStakeAccounts } from "~/server/getStakeAccounts";
+import { getSimulationComputeUnits, getStakeAccounts } from "~/server/server";
+const walletPublicKey = new PublicKey(process.env.VITE_WALLET as string);
 
 export default function Home() {
   const stakeAccounts = createAsync(getStakeAccounts)
+
+  const sendSolComputeUnits = createAsync(async () => {
+    return await getSimulationComputeUnits(
+      "Send SOL Compute Units",
+      [SystemProgram.transfer({
+        fromPubkey: walletPublicKey,
+        toPubkey: walletPublicKey,
+        lamports: 0,
+      })],
+      walletPublicKey,
+      [])
+  })
+
+  const sendUsdcComputeUnits = createAsync(async () => {
+    return await getSimulationComputeUnits(
+      "Send USDC Compute Units",
+      [createTransferCheckedInstruction(
+        new PublicKey("GZTwiZ4t2KTfpV8bvKGEL3nvXrbiKZEo8zF4639VNUSE"),
+        new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+        new PublicKey("GZTwiZ4t2KTfpV8bvKGEL3nvXrbiKZEo8zF4639VNUSE"),
+        walletPublicKey,
+        50_000,
+        6,
+      )],
+      walletPublicKey,
+      [])
+  })
+
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
@@ -12,45 +43,25 @@ export default function Home() {
           <span class="mr-5">Hello</span> <img class="w-12 h-12" src="https://unocss.dev/logo.svg" alt="UnoCSS logo" />!
         </div>
       </h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <For each={stakeAccounts()}>{
-          (account) => <div class="text-left">
-            <p  >Account: {account.account}</p>
-            <p>Lamports: {account.lamports}</p>
-          </div>
-        }</For>
+
+      <Suspense fallback={<p>Loading Compute Units...</p>}>
+        <p>Send SOL Compute Units: {sendSolComputeUnits()}</p>
+        <p>Send USDC Compute Units: {sendUsdcComputeUnits()}</p>
+      </Suspense>
+
+      <Suspense fallback={<p>Loading Stake Accounts...</p>}>
+        <div class="text-left">
+          <p>Your Account: {stakeAccounts()?.pubKey}</p>
+          <For each={stakeAccounts()?.accts}>{
+            (account) => <div>
+              <p>Account: {account.account}</p>
+              <p>Lamports: {account.lamports}</p>
+            </div>
+          }</For>
+        </div>
       </Suspense>
 
       <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="mt-2">
-        Visit{" "}
-        <a
-          href="https://unocss.dev"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          unocss.dev
-        </a>{" "}
-        to learn how to style your app.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <a href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </a>{" "}
-      </p>
     </main >
   );
 }
